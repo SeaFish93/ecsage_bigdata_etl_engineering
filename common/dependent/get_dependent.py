4bca3288bb20c5122dfbc6140053656ac11846cd
wangsong <wangsong@ecsage.com>
2020-09-24 10:00:54 +0800

    #修改项目名称

--- common/dependent/get_dependent.py
+++ common/dependent/get_dependent.py
@@ -63,7 +63,7 @@ def run(jd, **kwargs):
        #任务所有的依赖
        ok, get_list_depend_data = etl_md.execute_sql(sqlName="get_list_depend_sql", Parameter={"task_id": target_db + "_" + target_table},IsReturnData="Y")
        if len(get_list_depend_data) > 0:
-         print(get_list_depend_data[0],"=================################################")
+         print(get_list_depend_data,"=================################################")
 
 def replace_placeholder(txt):
     trx_dt = airflow.ds_nodash_utc8
