648f5557c82b8432a036df56033e01c95ab56864
wangsong <wangsong@ecsage.com>
2020-09-24 09:34:02 +0800

    #修改项目名称

--- sql/etl_meta/etl_metadata.py
+++ sql/etl_meta/etl_metadata.py
@@ -308,9 +308,9 @@ class EtlMetaDataSQL():
   """%("##{task_id}##","##{dep_task_id}##")
   #判断依赖是否在采集任务配置表或ETL任务配置表
   get_is_task_sql = """
-     select * from metadb.etl_tasks_info where task_id = '%s'
+     select 1 from metadb.etl_tasks_info where task_id = '%s'
         union all
-     select * from metadb.sync_tasks_info where task_id = '%s'
+     select 1 from metadb.sync_tasks_info where task_id = '%s'
   """%("##{task_id}##","##{task_id}##")
   #查找上游依赖
   get_upstream_depend_sql = """
