0735c35421cd52de69680c9ed9cc2d6ce1f58580
wangsong <wangsong@ecsage.com>
2020-10-09 11:43:00 +0800

    #

--- common/operator/beeline/beeline_operator.py
+++ common/operator/beeline/beeline_operator.py
@@ -12,12 +12,10 @@ import time
 class BeelineNoSqlDB(BaseDB):
     def __init__(self, host=None, port=None, user=None, password=None, metastore_uris=None):
         super().__init__(host=host, port=port, user=user, password=password)
-        host = "bd37-dnode4:2181,bd19-dnode2:2181,bd26-dnode3:2181"
-        metastore_uris = "jdbc:hive2://%s/default;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2_zk"%(host)
         self.metastore_uris = metastore_uris
         print("beeline NoSql DB:" + self.metastore_uris)
         #modify by wangsong（source /etc/profile）
-        self.conn = "beeline -u '%s' -n %s -d org.apache.hive.jdbc.HiveDriver -p '%s'" % (self.metastore_uris, self.user, self.password)
+        self.conn = "/opt/cloudera/parcels/CDH/lib/hive/bin/beeline -u 'jdbc:hive2://%s/' -n %s -d org.apache.hive.jdbc.HiveDriver -p '%s'" % (self.metastore_uris, self.user, self.password)
         # self.conn = "/usr/bin/beeline -u 'jdbc:hive2://%s/' -n %s " % (self.metastore_uris, self.user)
 
     def execute_sql(self, sql, task_name=""):
@@ -31,7 +29,7 @@ class BeelineNoSqlDB(BaseDB):
         f.flush()
         # add by wangsong（print sql）
         print(sql_set)
-        print("beeline exec_script sql：\n" + sql)
+        print("beeline exec sql：\n" + sql)
         res = os.system("%s -f %s" % (self.conn, sql_file))
         os.system("rm %s" % sql_file)
         f.close()
@@ -40,17 +38,18 @@ class BeelineNoSqlDB(BaseDB):
             return False
         else:
             return True
+        # os.system("beeline -u 'jdbc:hive2://cdh-master2:10000/' -n hive -d org.apache.hive.jdbc.HiveDriver -f %s" % sql_file)
 
     def get_password(self):
         return self.password
 
 
 """
-set hive.exec_script.parallel.thread.number=3;
+set hive.exec.parallel.thread.number=3;
 set mapred.max.split.size=256000000;
 set mapred.min.split.size.per.node=256000000;
 set mapred.min.split.size.per.rack=256000000;
-set hive.exec_script.reducers.bytes.per.reducer=256000000;
+set hive.exec.reducers.bytes.per.reducer=256000000;
 set hive.input.format=org.apache.hadoop.hive.ql.io.CombineHiveInputFormat;
-set hive.exec_script.parallel=true;
+set hive.exec.parallel=true;
 """
