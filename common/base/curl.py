5f22772dd42bd3bdf5499f20772d02a5d4ad3433
wangsong <wangsong@ecsage.com>
2020-10-13 17:02:03 +0800

    #

--- common/base/curl.py
+++ common/base/curl.py
@@ -10,16 +10,29 @@ import json
 from ecsage_bigdata_etl_engineering.common.alert.alert_info import get_create_dag_alert
 from ecsage_bigdata_etl_engineering.common.base.set_process_exit import set_exit
 import os
+import time
 
-def exec_interface_data_curl(URL="",Data={}):
+def exec_interface_data_curl(URL="",Data={},File=""):
     headers = {'Content-Type': "application/json"}
     try:
         response = requests.post(URL, data=json.dumps(Data), headers=headers)
-        if response.status_code == 404:
-            msg = get_create_dag_alert(FileName="%s" % (os.path.basename(__file__)),
-                                       Log="执行数据接口采集出现异常！！！",
-                                       Developer="工程维护")
-            set_exit(LevelStatu="red", MSG=msg)
+        while True:
+          is_md5 = os.path.exists("%s.md5"%(File))
+          if is_md5:
+            is_file = os.path.exists("%s"%(File))
+            if is_file:
+               file_md5 = os.popen("md5sum %s"%(File))
+               file_md5_value = file_md5.read().split()[0]
+               md5_file_md5 = os.popen("cat %s.md5"%(File))
+               md5_file_md5_value = md5_file_md5.read().split()[0]
+               if file_md5_value != md5_file_md5_value:
+                   msg = get_create_dag_alert(FileName="%s" % (os.path.basename(__file__)),
+                                              Log="执行数据接口采集出现异常！！！",
+                                              Developer="工程维护")
+                   set_exit(LevelStatu="red", MSG=msg)
+               else:
+                   exit(0)
+          time.sleep(120)
         return response.status_code
     except Exception as e:
         msg = get_create_dag_alert(FileName="%s" % (os.path.basename(__file__)), Log="执行数据接口采集出现异常！！！",
