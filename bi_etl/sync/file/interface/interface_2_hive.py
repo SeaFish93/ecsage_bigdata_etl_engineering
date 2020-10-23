ed4a5fb7f77b2fc795d4ee5563e470962ef76245
wangsong <wangsong@ecsage.com>
2020-10-23 16:42:15 +0800

    #

--- bi_etl/sync/file/interface/interface_2_hive.py
+++ bi_etl/sync/file/interface/interface_2_hive.py
@@ -137,21 +137,23 @@ def get_file_2_hive(HiveSession="",BeelineSession="",InterfaceUrl="",DataJson={}
            md5_file_false.append(file[0])
         else:
            pass
-      if len(md5_file_false) > 0 and sleep_num <= 60:
+      if len(md5_file_false) > 0:
+          if sleep_num <= 60:
               print("等待第%s次"%(sleep_num))
               time.sleep(60)
+          else:
+              msg = "等待md5生成超时！！！\n%s" % (md5_file_false)
+              msg = get_alert_info_d(DagId=airflow.dag, TaskId=airflow.task,
+                                     SourceTable="%s.%s" % ("SourceDB", "SourceTable"),
+                                     TargetTable="%s.%s" % (DB, Table),
+                                     BeginExecDate=ExecData,
+                                     EndExecDate=ExecData,
+                                     Status="Error",
+                                     Log=msg,
+                                     Developer="developer")
+              set_exit(LevelStatu="red", MSG=msg)
       else:
-          msg = "等待md5生成超时！！！\n%s"%(md5_file_false)
           set_md5_file_true = False
-          msg = get_alert_info_d(DagId=airflow.dag, TaskId=airflow.task,
-                                 SourceTable="%s.%s" % ("SourceDB", "SourceTable"),
-                                 TargetTable="%s.%s" % (DB, Table),
-                                 BeginExecDate=ExecData,
-                                 EndExecDate=ExecData,
-                                 Status="Error",
-                                 Log=msg,
-                                 Developer="developer")
-          set_exit(LevelStatu="red", MSG=msg)
       md5_file_false.clear()
       sleep_num = sleep_num + 1
     #落地临时表
