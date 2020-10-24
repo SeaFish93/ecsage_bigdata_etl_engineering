cd564c7bd4b176c04182e1143f29cc314988a7d4
wangsong <wangsong@ecsage.com>
2020-10-24 12:37:30 +0800

    #

--- dags/data_etl_script.py
+++ dags/data_etl_script.py
@@ -132,16 +132,14 @@ for dag_info in get_dags:
                          if len(task_downstream_deps) == 0:
                              task[task_dep[1]].set_upstream(start_etl_task)
                      else:
-                         try:
                            external_task = PythonOperator(task_id='external_%s_%s' % (task_dep[0], task_dep[1]),
                                                         python_callable=dep_task_main,
                                                         provide_context=True,
                                                         op_args=(task_dep[0], task_dep[1], task_dep[4],),
                                                         dag=dag)
+                           print(external_task.task_id,"====================================================")
                            task[task_dep[2]].set_upstream(external_task)
                            external_task.set_upstream(start_etl_task)
-                         except Exception as e:
-                           print("===============================================")
              else:
                  task['%s' % (task_name["task_id"])].set_upstream(start_etl_task)
              ok, task_upstream_deps = etl_md.execute_sql(sqlName="get_upstream_depend_sql", Parameter={"dep_task_id": task_name["task_id"]},IsReturnData="Y")
