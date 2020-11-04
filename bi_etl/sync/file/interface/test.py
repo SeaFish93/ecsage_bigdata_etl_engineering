0d07d978ba21f96ba81547026ebb132624990455
wangsong <wangsong@ecsage.com>
2020-11-04 15:23:34 +0800

    #

--- bi_etl/sync/file/interface/test.py
+++ bi_etl/sync/file/interface/test.py
@@ -219,22 +219,96 @@ def set_download_content(AccountId="",TaskId="",Token=""):
     print(resp_data, "=========================================")
 
     if resp_data == "empty result":
-        os.system("""echo "%s %s %s">>/tmp/empty.log """%(AccountId,TaskId,Token))
+        os.system("""echo "%s %s %s">>/tmp/empty1.log """%(AccountId,TaskId,Token))
     else:
-        os.system("""echo "%s">>/tmp/notempty.log """%(resp_data))
+        os.system("""echo "%s">>/tmp/notempty1.log """%(resp_data))
 
-def get_download_content():
-    ok,datas = etl_md.get_all_rows("select * from metadb.oe_async_task_interface_bak1")
+def get_download_content(Sql="",arg=None):
+  if arg is not None:
+    Sql = arg["Sql"]
+    print(Sql,"################################")
+    ok,datas = etl_md.get_all_rows(Sql)
     for data in datas:
-        token = data[1]
-        service_code = data[2]
-        account_id = data[3]
-        task_id = data[4]
-        task_name = data[5]
+        token = data[0]
+        service_code = data[1]
+        account_id = data[2]
+        task_id = data[3]
+        task_name = data[4]
         set_download_content(AccountId=account_id, TaskId=task_id, Token=token)
 
+def get_download_task():
+    sql_list = get_download_sql(ServiceCode="tt-hnhd-02")
+    if sql_list is not None and len(sql_list) > 0:
+        i = 0
+        th = []
+        for sqls in sql_list:
+                i = i + 1
+                etl_thread = EtlThread(thread_id=i, thread_name="Thread.%d" % (i),
+                                   my_run=get_download_content,
+                                   Sql = sqls
+                                   )
+                etl_thread.start()
+                import time
+                time.sleep(2)
+                th.append(etl_thread)
+        for etl_th in th:
+            etl_th.join()
+        print("the end!!!!!")
+
+def get_download_sql(ServiceCode=""):
+    #获取子账户
+    source_data_sql = """
+                   select token_data,service_code,account_id,task_id,task_name from(
+                   select token_data,service_code,account_id,task_id,task_name,@row_num:=@row_num+1 as rn
+                   from metadb.oe_async_task_interface_bak1 a,(select @row_num:=0) r
+                   where service_code = '%s'
+                   ) tmp
+                """%(ServiceCode)
+    #获取子账户条数
+    get_account_count_sql = """
+       select count(1),min(rn),max(rn) 
+       from(select token_data,service_code,account_id,task_id,task_name,@row_num:=@row_num+1 as rn
+            from metadb.oe_async_task_interface_bak1 a,(select @row_num:=0) r
+            where service_code = '%s'
+           ) tmp
+    """%(ServiceCode)
+    ok,all_rows = etl_md.get_all_rows(get_account_count_sql)
+    fcnt = 0
+    sql_list = []
+    if all_rows is not None and len(all_rows) > 0:
+        fcnt = all_rows[0][0]
+        fmin = int(all_rows[0][1])
+        fmax = int(all_rows[0][2])
+    if fcnt > 0:
+        source_cnt = fcnt
+        print("min=%s, max=%s, count=%s" % (str(fmin), str(fmax), str(fcnt)))
+        if fcnt < 500:
+            # 500以下的数据量不用分批跑
+            sql_list.clear()
+            sql_list.append(source_data_sql)
+        else:
+            sql_list.clear()
+            num_proc = int(fmax) - int(fmin)
+            if num_proc > 5:
+                # 最多5个进程同时获取数据
+                num_proc = 20
+            #if fcnt > 10000:
+            #    num_proc = 20
+            # 每一个进程查询量的增量
+            d = math.ceil((int(fmax) - int(fmin) + 1) / num_proc)
+            i = 0
+            while i < num_proc:
+                s_ind = int(fmin) + i * d
+                e_ind = s_ind + d
+                if i == num_proc - 1:
+                    e_ind = int(fmax) + 1
+                sql = source_data_sql + " where rn" + " >= " + str(s_ind) + " and rn" + " < " + str(e_ind)
+                sql_list.append(sql)
+                i = i + 1
+    return sql_list
+
 if __name__ == '__main__':
-    get_download_content()
+    get_download_task()
     #etl_md.execute_sql("""delete from metadb.oe_async_task_interface where dag_id='%s' and dag_task_id = '%s'""" % ("test", "test"))
     ##########os.system("""rm -f /tmp/task_status_1.log """)
     ##########os.system("""rm -f /tmp/create_task_status_1.log""")
