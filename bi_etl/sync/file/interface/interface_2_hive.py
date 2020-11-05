b6194bc1015ea2d4060fbe6deb7fcd14bd0a8418
wangsong <wangsong@ecsage.com>
2020-11-05 16:54:03 +0800

    #

--- bi_etl/sync/file/interface/interface_2_hive.py
+++ bi_etl/sync/file/interface/interface_2_hive.py
@@ -71,7 +71,7 @@ def main(TaskInfo, Level,**kwargs):
       get_file_2_hive(HiveSession=hive_session,BeelineSession=beeline_session,InterfaceUrl=interface_url,DataJson=data_json
                       ,FileDirName = file_dir_name,DataJsonRequest=data_json_request
                       ,InterfaceModule = interface_module,CommitNum=commit_num
-                      ,DB=target_db, Table=target_table,ExecData=exec_date
+                      ,DB=target_db, Table=target_table,ExecDate=exec_date
                      )
     elif Level == "ods":
       exec_ods_hive_table(HiveSession=hive_session,BeelineSession=beeline_session,SourceDB=source_db,SourceTable=source_table,
@@ -84,17 +84,10 @@ def main(TaskInfo, Level,**kwargs):
 def get_file_2_hive(HiveSession="",BeelineSession="",InterfaceUrl="",DataJson={},DataJsonRequest=""
                                    ,FileDirName = ""
                                    ,InterfaceModule = "",CommitNum=""
-                                   ,DB="", Table="",ExecData=""
+                                   ,DB="", Table="",ExecDate=""
                                    ):
     data_json = DataJson
-    mysql_session = set_db_session(SessionType="mysql", SessionHandler="mysql_media")
-    ok,data_list = mysql_session.get_all_rows("""select account_id, media, service_code 
-                                                 from big_data_mdg.media_advertiser 
-                                                 where media = %s
-                                                   and service_code = 'tt-hnhd-12'
-                                                   and system_status in ('STATUS_ENABLE','STATUS_PENDING_CONFIRM_MODIFY','STATUS_WAIT_FOR_PUBLIC_AUTH')
-                                              """%(int(data_json["mt"])))
-    #ok, data_list = mysql_session.get_all_rows("""select account_id, media, service_code from big_data_mdg.media_advertiser where media =2 and account_id in (%s)""" % (data))
+    etl_md = set_db_session(SessionType="mysql", SessionHandler="etl_metadb")
     request_type = data_json["mt"]
     if "filtering" in data_json.keys():
        if "status" in data_json["filtering"].keys():
@@ -103,77 +96,123 @@ def get_file_2_hive(HiveSession="",BeelineSession="",InterfaceUrl="",DataJson={}
            delete_type = "not_delete"
     else:
        delete_type = "not_delete"
-    num = 1
-    nums = 1
-    run_num = 0
-    request_params = []
     print("开始执行调用接口")
     data_dir = conf.get("Interface", InterfaceModule)
     file_dir = "%s" % (data_dir) + "/" + airflow.ds_nodash_utc8 + "/%s/%s" % (airflow.dag,request_type)
     ########set_filebeat_yml(DataFile=file_dir, FileName="%s_%s_%s.log" % (airflow.dag, airflow.task,ExecData), ExecDate=ExecData)
     #数据文件绝对路径
     file_dir_name_list = []
-    advertiser_list = []
-    for data in data_list:
-       request_params.append(data)
-       advertiser_list.append({"serviceCode":str(data[2]),"accountId":str(data[0])})
-       if num == CommitNum or nums == len(data_list):
-          run_num = run_num + 1
-          print("第%s批正在提交！%s"%(run_num,advertiser_list))
-          now_time = time.strftime("%H_%M_%S", time.localtime())
-          file_name = "%s_%s_%s_%s_%s.log" % (airflow.dag, airflow.task,run_num,ExecData, now_time)
-          file_dir_name = "%s/%s" % (file_dir, file_name)
-          if os.path.exists(file_dir) is False:
-              os.system("mkdir -p %s" % (file_dir))
-          data_json["%s" % (FileDirName)] = file_dir_name
-          data_json["advertiser_list"] = advertiser_list
-          file_dir_name_list.append((file_dir_name, data_json))
-          print("请求接口URL：%s"%(InterfaceUrl))
-          print("请求接口参数：%s"%(data_json))
-          # 分子账户开启进程
-          exec_interface_data_curl(URL=InterfaceUrl, Data=data_json, File=file_dir_name,DataJsonRequest=DataJsonRequest)
-          #time.sleep(10)
-          num = 0
-          advertiser_list = []
-       num = num + 1
-       nums = nums + 1
+    #获取请求域名个数
+    host_count_sql = """
+         select count(1)
+         from metadb.request_hostname_interface a
+    """
+    ok, host_count = etl_md.get_all_rows(host_count_sql)
+    #获取每个域名分配的子账户个数
+    account_num_sql = """
+        select count(1),min(rn),max(rn) from(
+        select account_id, media_type, service_code,@row_num:=@row_num+1 as rn
+        from metadb.oe_valid_account_interface a,(select @row_num:=0) r
+        where media_type = %s
+        ) tmp
+    """%(int(data_json["mt"]))
+    ok, account_num = etl_md.get_all_rows(account_num_sql)
+    account_avg = account_num[0][0] / host_count[0][0]
+    if account_avg > 0:
+        account_avg = account_avg + 1
+        account_run_num = 1
+        host = ""
+        for n in range(1, host_count[0][0]+1):
+           #获取请求hostname
+           host_sql = """
+              select hostname
+              from(select hostname,@row_num:=@row_num+1 as rn
+                   from metadb.request_hostname_interface a,(select @row_num:=0) r
+                  ) tmp
+              where rn = %s
+           """%(n)
+           ok, hosts = etl_md.get_all_rows(host_sql)
+           host = hosts[0][0]
+           #请求子账户
+           account_sql = """
+                   select account_id, media_type, service_code from(
+                   select account_id, media_type, service_code,@row_num:=@row_num+1 as rn
+                   from metadb.oe_valid_account_interface a,(select @row_num:=0) r
+                   where media_type = %s
+                   ) tmp
+                   where rn >= %s and rn <= %s
+               """ % (int(data_json["mt"]),account_run_num, account_avg)
+           ok, accounts_list = etl_md.get_all_rows(account_sql)
+           #提交请求
+           interface_url = """http://%s%s"""%(host,InterfaceUrl)
+           file = request_commit_account(AccountData=accounts_list, Num=n, InterfaceUrl=interface_url, ExecDate=ExecDate, FileDir=file_dir,
+                                  FileDirName=FileDirName,DataJson="")
+           file_dir_name_list.append(file)
+           #提交次数
+           account_run_num = account_avg + 1
+           account_avg = account_run_num + account_avg
+
+    ######### for data in data_list:
+    #########    request_params.append(data)
+    #########    advertiser_list.append({"serviceCode":str(data[2]),"accountId":str(data[0])})
+    #########    if num == CommitNum or nums == len(data_list):
+    #########       run_num = run_num + 1
+    #########       print("第%s批正在提交！%s"%(run_num,advertiser_list))
+    #########       now_time = time.strftime("%H_%M_%S", time.localtime())
+    #########       file_name = "%s_%s_%s_%s_%s.log" % (airflow.dag, airflow.task,run_num,ExecDate, now_time)
+    #########       file_dir_name = "%s/%s" % (file_dir, file_name)
+    #########       if os.path.exists(file_dir) is False:
+    #########           os.system("mkdir -p %s" % (file_dir))
+    #########       data_json["%s" % (FileDirName)] = file_dir_name
+    #########       data_json["advertiser_list"] = advertiser_list
+    #########       file_dir_name_list.append((file_dir_name, data_json))
+    #########       print("请求接口URL：%s"%(InterfaceUrl))
+    #########       print("请求接口参数：%s"%(data_json))
+    #########       # 分子账户开启进程
+    #########       exec_interface_data_curl(URL=InterfaceUrl, Data=data_json, File=file_dir_name,DataJsonRequest=DataJsonRequest)
+    #########       #time.sleep(10)
+    #########       num = 0
+    #########       advertiser_list = []
+    #########    num = num + 1
+    #########    nums = nums + 1
     print("结束执行调用接口，进行等待MD5文件生成")
-    md5_file_false = []
-    set_md5_file_true = True
-    sleep_num = 1
-    print(file_dir_name_list,"========================================================")
-    # 判断是否没有md5的文件
-    while set_md5_file_true:
-      for file in file_dir_name_list:
-        #判断md5文件是否存在
-        is_md5_file = os.path.exists(file[0]+".md5")
-        if is_md5_file is False:
-           md5_file_false.append(file[0])
-        else:
-           pass
-      if len(md5_file_false) > 0:
-          wait_mins = 600
-          if sleep_num <= wait_mins:
-              min = 60
-              print("等待第%s次%s秒"%(sleep_num,min))
-              time.sleep(min)
-          else:
-              msg = "等待md5生成超时！！！\n%s" % (md5_file_false)
-              msg = get_alert_info_d(DagId=airflow.dag, TaskId=airflow.task,
-                                     SourceTable="%s.%s" % ("SourceDB", "SourceTable"),
-                                     TargetTable="%s.%s" % (DB, Table),
-                                     BeginExecDate=ExecData,
-                                     EndExecDate=ExecData,
-                                     Status="Error",
-                                     Log=msg,
-                                     Developer="developer")
-              set_exit(LevelStatu="red", MSG=msg)
-      else:
-          set_md5_file_true = False
-      md5_file_false.clear()
-      sleep_num = sleep_num + 1
+    wait_for_md5(FileDirNameList=file_dir_name_list, DB=DB, Table=Table, ExecDate=ExecDate)
+    #### md5_file_false = []
+    #### set_md5_file_true = True
+    #### sleep_num = 1
+    #### print(file_dir_name_list,"========================================================")
+    #### # 判断是否没有md5的文件
+    #### while set_md5_file_true:
+    ####   for file in file_dir_name_list:
+    ####     #判断md5文件是否存在
+    ####     is_md5_file = os.path.exists(file[0]+".md5")
+    ####     if is_md5_file is False:
+    ####        md5_file_false.append(file[0])
+    ####     else:
+    ####        pass
+    ####   if len(md5_file_false) > 0:
+    ####       wait_mins = 600
+    ####       if sleep_num <= wait_mins:
+    ####           min = 60
+    ####           print("等待第%s次%s秒"%(sleep_num,min))
+    ####           time.sleep(min)
+    ####       else:
+    ####           msg = "等待md5生成超时！！！\n%s" % (md5_file_false)
+    ####           msg = get_alert_info_d(DagId=airflow.dag, TaskId=airflow.task,
+    ####                                  SourceTable="%s.%s" % ("SourceDB", "SourceTable"),
+    ####                                  TargetTable="%s.%s" % (DB, Table),
+    ####                                  BeginExecDate=ExecData,
+    ####                                  EndExecDate=ExecData,
+    ####                                  Status="Error",
+    ####                                  Log=msg,
+    ####                                  Developer="developer")
+    ####           set_exit(LevelStatu="red", MSG=msg)
+    ####   else:
+    ####       set_md5_file_true = False
+    ####   md5_file_false.clear()
+    ####   sleep_num = sleep_num + 1
     #落地临时表
-    exec_file_2_hive(HiveSession=HiveSession,BeelineSession=BeelineSession,LocalFileName=file_dir_name_list,RequestType=request_type,DeleteType=delete_type,DB=DB,Table=Table,ExecDate=ExecData)
+    exec_file_2_hive(HiveSession=HiveSession,BeelineSession=BeelineSession,LocalFileName=file_dir_name_list,RequestType=request_type,DeleteType=delete_type,DB=DB,Table=Table,ExecDate=ExecDate)
 
 def exec_file_2_hive(HiveSession="",BeelineSession="",LocalFileName="",RequestType="",DeleteType="",DB="",Table="",ExecDate=""):
     mid_table = """%s.%s""" % (DB, Table)
@@ -532,7 +571,7 @@ def exec_snap_hive_table(HiveSession="",BeelineSession="",SourceDB="",SourceTabl
         select %s
         from %s.%s a where etl_date = '%s' 
        """%(TargetDB,TargetTable,snap_columns,TargetDB,TargetTable,ExecDate,snap_columns,SourceDB,SourceTable,ExecDate)
-   ok = HiveSession.execute_sql(sql)
+   ok = BeelineSession.execute_sql(sql)
    if ok is False:
        msg = get_alert_info_d(DagId=airflow.dag, TaskId=airflow.task,
                               SourceTable="%s.%s" % (SourceDB, SourceTable),
@@ -555,8 +594,9 @@ def exec_snap_hive_table(HiveSession="",BeelineSession="",SourceDB="",SourceTabl
                on 1 = 1
                where a.source_cnt <> b.target_cnt
                """%(SourceDB,SourceTable,ExecDate,TargetDB,TargetTable,ExecDate)
-   ok, data = HiveSession.get_all_rows(sql_check)
+   #ok, data = HiveSession.get_all_rows(sql_check)
    data = []
+   ok = True
    print("snap入库数据：" + str(data))
    if ok is False or len(data) > 0:
        msg = get_alert_info_d(DagId=airflow.dag, TaskId=airflow.task,
@@ -600,3 +640,59 @@ def set_filebeat_yml(DataFile="",FileName="",ExecDate=""):
     os.system(sed_file_dir)
     scp = """ scp %s root@bd127-node:/etc/filebeat/inputs.d/ """%(yml_file)
     os.system(scp)
+
+#等待md5
+def wait_for_md5(FileDirNameList="",DB="", Table="",ExecDate=""):
+    md5_file_false = []
+    set_md5_file_true = True
+    sleep_num = 1
+    # 判断是否没有md5的文件
+    while set_md5_file_true:
+      for file in FileDirNameList:
+        #判断md5文件是否存在
+        is_md5_file = os.path.exists(file[0]+".md5")
+        if is_md5_file is False:
+           md5_file_false.append(file[0])
+        else:
+           pass
+      if len(md5_file_false) > 0:
+          wait_mins = 600
+          if sleep_num <= wait_mins:
+              min = 60
+              print("等待第%s次%s秒"%(sleep_num,min))
+              time.sleep(min)
+          else:
+              msg = "等待md5生成超时！！！\n%s" % (md5_file_false)
+              msg = get_alert_info_d(DagId=airflow.dag, TaskId=airflow.task,
+                                     SourceTable="%s.%s" % ("SourceDB", "SourceTable"),
+                                     TargetTable="%s.%s" % (DB, Table),
+                                     BeginExecDate=ExecDate,
+                                     EndExecDate=ExecDate,
+                                     Status="Error",
+                                     Log=msg,
+                                     Developer="developer")
+              set_exit(LevelStatu="red", MSG=msg)
+      else:
+          set_md5_file_true = False
+      md5_file_false.clear()
+      sleep_num = sleep_num + 1
+
+def request_commit_account(AccountData="",Num="",InterfaceUrl="",ExecDate="",FileDir="",FileDirName="",DataJson=""):
+    advertiser_list = []
+    for data in AccountData:
+        account_id = str(data[0])
+        service_code = str(data[2])
+        advertiser_list.append({"serviceCode": service_code, "accountId": account_id})
+    print("第%s批正在提交！%s" % (Num, advertiser_list))
+    now_time = time.strftime("%H_%M_%S", time.localtime())
+    file_name = "%s_%s_%s_%s_%s.log" % (airflow.dag, airflow.task, run_num, ExecDate, now_time)
+    file_dir_name = "%s/%s" % (FileDir, file_name)
+    if os.path.exists(FileDir) is False:
+       os.system("mkdir -p %s" % (FileDir))
+    DataJson["%s" % (FileDirName)] = file_dir_name
+    DataJson["advertiser_list"] = advertiser_list
+    print("请求接口URL：%s" % (InterfaceUrl))
+    print("请求接口参数：%s" % (DataJson))
+    # 分子账户开启进程
+    exec_interface_data_curl(URL=InterfaceUrl, Data=DataJson, File=file_dir_name, DataJsonRequest="DataJsonRequest")
+    return file_dir_name
