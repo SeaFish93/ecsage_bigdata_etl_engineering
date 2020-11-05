ffbd68c4b49a5a7614cec788f81cd3eb9828f822
wangsong <wangsong@ecsage.com>
2020-11-05 10:03:33 +0800

    #

--- bi_etl/sync/file/interface/test.py
+++ bi_etl/sync/file/interface/test.py
@@ -200,7 +200,8 @@ def set_download_content(AccountId="",TaskId="",Token=""):
     resp = requests.get(url, json=params, headers=headers)
     resp_data = resp.json()
     file_size = resp_data["data"]["list"][0]["file_size"]
-    print(file_size,"=============================================")
+    task_status = resp_data["data"]["list"][0]["task_status"]
+    print(file_size,task_status,"=============================================")
     if int(file_size) <= 12:
         os.system("""echo "%s %s %s">>/tmp/empty2.log """%(AccountId,TaskId,Token))
     else:
