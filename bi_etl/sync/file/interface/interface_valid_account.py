71f894ee961227272b8eda05480e3370380bb477
wangsong <wangsong@ecsage.com>
2020-11-06 18:23:28 +0800

    #

--- bi_etl/sync/file/interface/interface_valid_account.py
+++ bi_etl/sync/file/interface/interface_valid_account.py
@@ -415,8 +415,8 @@ if __name__ == '__main__':
     start_end_list = []
     print(max_min_test,"&&****************************************")
     for get_data in max_min_test:
-        start_end_list.append(max_min[n])
-        if len(start_end_list) == 5 or len(max_min_test) < 5:
+        start_end_list.append(max_min_test[n])
+        if len(start_end_list) == 5:
            print("[%s]执行机器" % (host_data[host_i][0]))
            print(start_end_list)
            host_i = host_i + 1
