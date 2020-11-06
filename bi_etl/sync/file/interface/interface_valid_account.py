7124bf0884a58b7c5c4522cb5aa36d23cd0a5db8
wangsong <wangsong@ecsage.com>
2020-11-06 11:44:45 +0800

    #

--- bi_etl/sync/file/interface/interface_valid_account.py
+++ bi_etl/sync/file/interface/interface_valid_account.py
@@ -9,47 +9,57 @@ from ecsage_bigdata_etl_engineering.common.base.etl_thread import EtlThread
 mysql_session = set_db_session(SessionType="mysql", SessionHandler="mysql_media")
 etl_md = set_db_session(SessionType="mysql", SessionHandler="etl_metadb")
 
-def get_account_sql(MediaType="",ServiceCode=""):
-    #获取子账户
-    source_data_sql = """
-                   select account_id, media, service_code from(
-                   select account_id, media, service_code,@row_num:=@row_num+1 as rn
-                   from big_data_mdg.media_advertiser a,(select @row_num:=0) r
-                   where media = %s 
-                     and service_code = '%s'
-                   ) tmp
-                """%(MediaType,ServiceCode)
-    #获取子账户条数
-    get_account_count_sql = """
-       select count(1),min(rn),max(rn) 
-       from(select account_id, media, service_code,@row_num:=@row_num+1 as rn
+def get_account_sql(MediaType=""):
+    #保存子账户
+    account_file = "/tmp/oe_request_get_account_%s.log"%(MediaType)
+    account_2_mysql_sql = """
+       select concat_ws(' ',@row_num:=@row_num+1,account_id, media, service_code)
             from big_data_mdg.media_advertiser a,(select @row_num:=0) r
             where media = %s
-              and service_code = '%s'
-           ) tmp
-    """%(MediaType,ServiceCode)
-    ok,all_rows = mysql_session.get_all_rows(get_account_count_sql)
+       """ % (MediaType)
+    os.system("rm -f %s"%(account_file))
+    mysql_session.select_data_to_local_file(sql=account_2_mysql_sql,filename=account_file)
+    insert_sql = """
+        load data local infile '%s' into table metadb.request_account_interface fields terminated by ' ' lines terminated by '\\n' (id,account_id,media_type,service_code)
+      """ % (account_file)
+    etl_md.execute_sql("""delete from metadb.request_account_interface where media_type = %s"""%(MediaType))
+    etl_md.local_file_to_mysql(sql=insert_sql)
+    #获取子账户
+    source_data_sql = """
+           select b.id,b.account_id,b.media_type,b.service_code
+           from metadb.request_account_token_interface a
+           inner join metadb.request_account_interface b
+           on a.service_code = b.service_code
+           and a.media_type = b.media_type
+           where a.media_type = %s
+    """%(MediaType)
+    #获取子账户条数
+    get_account_count_sql = """
+       select count(1),min(id),max(id) 
+       from metadb.request_account_interface
+       where media = %s
+    """%(MediaType)
+    ok,all_rows = etl_md.get_all_rows(get_account_count_sql)
     fcnt = 0
     sql_list = []
+    max_min = []
     if all_rows is not None and len(all_rows) > 0:
         fcnt = all_rows[0][0]
+    if fcnt > 0:
         fmin = int(all_rows[0][1])
         fmax = int(all_rows[0][2])
-    if fcnt > 0:
         source_cnt = fcnt
         print("min=%s, max=%s, count=%s" % (str(fmin), str(fmax), str(fcnt)))
-        if fcnt < 500:
-            # 500以下的数据量不用分批跑
+        if fcnt < 100:
+            # 100以下的数据量不用分批跑
             sql_list.clear()
             sql_list.append(source_data_sql)
         else:
             sql_list.clear()
             num_proc = int(fmax) - int(fmin)
-            if num_proc > 5:
-                # 最多5个进程同时获取数据
-                num_proc = 20
-            #if fcnt > 10000:
-            #    num_proc = 20
+            if num_proc > 4:
+                # 最多20个进程同时获取数据
+                num_proc = 4
             # 每一个进程查询量的增量
             d = math.ceil((int(fmax) - int(fmin) + 1) / num_proc)
             i = 0
@@ -58,10 +68,11 @@ def get_account_sql(MediaType="",ServiceCode=""):
                 e_ind = s_ind + d
                 if i == num_proc - 1:
                     e_ind = int(fmax) + 1
-                sql = source_data_sql + " where rn" + " >= " + str(s_ind) + " and rn" + " < " + str(e_ind)
-                sql_list.append(sql)
+                sql = source_data_sql + " and b.id" + " >= " + str(s_ind) + " and b.id" + " < " + str(e_ind)
+                #sql_list.append(sql)
+                max_min.append([s_ind,e_ind])
                 i = i + 1
-    return sql_list
+    return source_data_sql,max_min
 def get_account_token(MediaType="",ServiceCode="",AccountTokenFile="",AccountTokenExceptionFile=""):
     # 获取子账户
     source_data_sql = """
@@ -148,9 +159,9 @@ def create_task(Sql="",ThreadName="",Token="",MediaType="",ServiceCode="",AsyncT
                     os.system("""echo "%s %s %s %s">>%s """%(service_code,token_data,service_code,account_id,AsyncTaskExceptionFile))
                     set_true = False
                n = n + 1
-
+#创建任务
 def exec_create_task(MediaType="",ServiceCode="",AsyncTaskFile="",AsyncTaskExceptionFile="",AsyncTask=""):
-    sql_list = get_token(MediaType=MediaType,ServiceCode=ServiceCode)
+    sql_list = get_account_sql(MediaType=MediaType)
     if sql_list is not None and len(sql_list) > 0:
         i = 0
         th = []
@@ -348,7 +359,10 @@ if __name__ == '__main__':
     account_token_exception_file = """/tmp/account_token_exception_file_%s.log"""%(media_type)
     os.system("""rm -f %s """ % (account_token_file))
     os.system("""rm -f %s """ % (account_token_exception_file))
+    #获取token
     get_token(MediaType=media_type, AccountTokenFile=account_token_file, AccountTokenExceptionFile=account_token_exception_file)
+    #获取每台服务处理数据量
+    sql,max_min = get_account_sql(MediaType=media_type)
     ####### media_type = sys.argv[1]
     ####### service_code = sys.argv[2]
     ####### async_task = sys.argv[3]
@@ -365,7 +379,7 @@ if __name__ == '__main__':
     ####### os.system("""rm -f %s"""%(async_empty_file))
     ####### os.system("""rm -f %s"""%(async_status_exception_file))
     ####### os.system("""rm -f %s"""%(async_task_exception_file))
-    ####### exec_create_task(MediaType=media_type,ServiceCode=service_code,AsyncTaskFile=async_task_file,AsyncTaskExceptionFile=async_task_exception_file,AsyncTask=async_task)
+    #exec_create_task(MediaType=media_type,ServiceCode=service_code,AsyncTaskFile=async_task_file,AsyncTaskExceptionFile=async_task_exception_file,AsyncTask=async_task)
     ####### print("开始启动下载内容!!!!!")
     ####### import time
     ####### time.sleep(120)
