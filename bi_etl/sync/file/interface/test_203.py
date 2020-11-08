f9a21c19be591f983647544a7182aae6de4bbd1a
王松 <wangsong@ecsage.com>
2020-11-08 07:47:43 -0500

    Update test_203.py

--- bi_etl/sync/file/interface/test_203.py
+++ bi_etl/sync/file/interface/test_203.py
@@ -171,8 +171,8 @@ def set_async_tasks(MediaType="",ServiceCode="",AccountId="",ThreadName="",Num="
         "task_type": "REPORT",
         "force": "true",
         "task_params": {
-            "start_date": "2020-11-06",
-            "end_date": "2020-11-06",
+            "start_date": "2020-11-07",
+            "end_date": "2020-11-07",
             "group_by": ["STAT_GROUP_BY_CAMPAIGN_ID"]
         }
     }
@@ -358,12 +358,13 @@ if __name__ == '__main__':
        os.system("""rm -f %s"""%(async_empty_file))
        os.system("""rm -f %s"""%(async_status_exception_file))
        os.system("""rm -f %s"""%(async_task_exception_file))
-       #exec_create_task(MediaType=media_type,ServiceCode=service_code,AsyncTaskFile=async_task_file,AsyncTaskExceptionFile=async_task_exception_file,AsyncTask=async_task)
+       exec_create_task(MediaType=media_type,ServiceCode=service_code,AsyncTaskFile=async_task_file,AsyncTaskExceptionFile=async_task_exception_file,AsyncTask=async_task)
        print("开始启动下载内容!!!!!")
        import time
        #time.sleep(5)
        try:
-        get_download_task(MediaType=media_type,ServiceCode=service_code,AsyncNotemptyFile=async_notempty_file,AsyncEmptyFile=async_empty_file,AsyncStatusExceptionFile=async_status_exception_file)
+        pass
+        #get_download_task(MediaType=media_type,ServiceCode=service_code,AsyncNotemptyFile=async_notempty_file,AsyncEmptyFile=async_empty_file,AsyncStatusExceptionFile=async_status_exception_file)
        except Exception as e:
            pass
        os.system("""date >>%s """%(async_date_file))
