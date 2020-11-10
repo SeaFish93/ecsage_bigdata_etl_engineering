4d30ce81192467fd73db21a12352b69c0662866b
wangsong <wangsong@ecsage.com>
2020-11-10 16:23:47 +0800

    #

--- bi_etl/sync/file/interface/set_create_oe_async_tasks.py
+++ bi_etl/sync/file/interface/set_create_oe_async_tasks.py
@@ -53,22 +53,24 @@ def main(TaskInfo,**kwargs):
                else:
                    min_n = 1
                sqls_list = get_run_sql(Sql=sql, Max=max, Min=min, Count=count, MinN=min_n)
-               shell_cmd = """
-                  python3 /root/bigdata_item_code/ecsage_bigdata_etl_engineering/bi_etl/sync/file/interface/create_async_tasks.py "%s" "%s" "%s" "%s" "%s" "%s" > /root/wangsong/create_async.log
-               """ % (media_type, async_task_name, sqls_list, async_task_file, async_task_exception_file,exec_date)
-               #exec_remote_proc(HostName=host_data[host_i][0], UserName=host_data[host_i][1], PassWord=host_data[host_i][2], ShellCommd=shell_cmd)
-               etl_thread = EtlThread(thread_id=n, thread_name="fetch%d" % (n),
-                                      my_run=exec_remote_proc,HostName=host_data[host_i][0],
-                                      UserName=host_data[host_i][1],PassWord=host_data[host_i][2], ShellCommd=shell_cmd
-                                      )
-               etl_thread.start()
-               th.append(etl_thread)
+               for sqls in sqls_list:
+                   os.system("""echo "%s">>/tmp/sqlsql.sql """%(sqls))
+               ##### shell_cmd = """
+               #####    python3 /root/bigdata_item_code/ecsage_bigdata_etl_engineering/bi_etl/sync/file/interface/create_async_tasks.py "%s" "%s" "%s" "%s" "%s" "%s" > /root/wangsong/create_async.log
+               ##### """ % (media_type, async_task_name, sqls_list, async_task_file, async_task_exception_file,exec_date)
+               ##### #exec_remote_proc(HostName=host_data[host_i][0], UserName=host_data[host_i][1], PassWord=host_data[host_i][2], ShellCommd=shell_cmd)
+               ##### etl_thread = EtlThread(thread_id=n, thread_name="fetch%d" % (n),
+               #####                        my_run=exec_remote_proc,HostName=host_data[host_i][0],
+               #####                        UserName=host_data[host_i][1],PassWord=host_data[host_i][2], ShellCommd=shell_cmd
+               #####                        )
+               ##### etl_thread.start()
+               ##### th.append(etl_thread)
            start_end_list = []
            host_i = host_i + 1
         host_num = host_num + 1
         n = n + 1
-    for etl_th in th:
-        etl_th.join()
+    ######for etl_th in th:
+    ######    etl_th.join()
 
 def get_run_sql(Sql="",Max="",Min="",Count="",MinN=""):
     fcnt = int(Count)
