8d02829c60ba9d3461e7b5a29ec03b9e9827e00b
wangsong <wangsong@ecsage.com>
2020-11-10 17:35:27 +0800

    #

--- bi_etl/sync/file/interface/set_create_oe_async_tasks.py
+++ bi_etl/sync/file/interface/set_create_oe_async_tasks.py
@@ -105,8 +105,8 @@ def get_run_sql(Sql="",Max="",Min="",Count="",LastNumber=""):
                 if e_ind <= Max and LastNumber[0] != LastNumber[1]:
                   sql = Sql + " and b.id" + " >= " + str(s_ind) + " and b.id" + " < " + str(e_ind)
                   sql_list.append(sql)
-                elif LastNumber[0] == LastNumber[1]:
-                    sql = Sql + " and b.id" + " >= " + str(s_ind) + " and b.id" + " <= " + str(e_ind)
+                elif LastNumber[0] == LastNumber[1] and i < num_proc-1:
+                    sql = Sql + " and b.id" + " >= " + str(s_ind) + " and b.id" + " < " + str(e_ind)
                     sql_list.append(sql)
                 i = i + 1
     return sql_list
