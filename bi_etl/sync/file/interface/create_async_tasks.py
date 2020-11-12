b190b67b3d85af1fc702ff489c4dcdf1b3880e04
王松 <wangsong@ecsage.com>
2020-11-12 09:40:36 -0500

    Update create_async_tasks.py

--- bi_etl/sync/file/interface/create_async_tasks.py
+++ bi_etl/sync/file/interface/create_async_tasks.py
@@ -69,6 +69,7 @@ def oe_run_create_task(MysqlSession="",Sql="",ThreadName="",AsyncTaskFile="",Asy
                set_true = False
              except Exception as e:
                if n > 3:
+                  os.system("""echo "%s %s %s %s %s %s">>%s """ % (media_type,token_data, service_code, account_id, 0, 9999,AsyncTaskFile))
                   os.system("""echo "%s %s %s %s %s">>%s """%(service_code,token_data,service_code,account_id,"%s%s" % (ThreadName, num),AsyncTaskExceptionFile))
                   print("""异常：%s %s %s"""%(token_data,service_code,account_id))
                   set_true = False
