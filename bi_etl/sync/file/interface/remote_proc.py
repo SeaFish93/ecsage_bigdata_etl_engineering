8edc50444ffb56038832b57de7834f6cb248fb50
wangsong <wangsong@ecsage.com>
2020-11-12 15:52:09 +0800

    #

--- bi_etl/sync/file/interface/remote_proc.py
+++ bi_etl/sync/file/interface/remote_proc.py
@@ -15,7 +15,7 @@ def exec_remote_proc(HostName="",UserName="",PassWord="",ShellCommd="",arg=None)
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     # 连接服务器
-    ssh.connect(hostname=HostName, port=22, username=UserName, password=PassWord)
+    ssh.connect(hostname=HostName, port=22, username=UserName, password=PassWord,timeout=120,look_for_keys=False)
     # 执行命令
     stdin, stdout, stderr = ssh.exec_command("""%s"""%(ShellCommd))
     # 获取命令结果
