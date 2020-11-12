b733788265bccc9c8e16f3987876a2ae4cfd7c16
王松 <wangsong@ecsage.com>
2020-11-12 08:40:38 -0500

    Update set_create_oe_async_tasks.py

--- bi_etl/sync/file/interface/set_create_oe_async_tasks.py
+++ bi_etl/sync/file/interface/set_create_oe_async_tasks.py
@@ -100,7 +100,7 @@ def get_run_sql(Sql="",Max="",Min="",Count="",LastNumber=""):
                e_ind = s_ind + d
                if i == num_proc - 1:
                   e_ind = int(fmax) + 1
-               sql = Sql + " where " + get_table_index + ">=" + str(s_ind) + " and " + get_table_index + "<" + str(e_ind)
+               sql = Sql + " where " + "get_table_index" + ">=" + str(s_ind) + " and " + "get_table_index" + "<" + str(e_ind)
                sql_list.append(sql)
                i = i + 1
             #while i < num_proc:
