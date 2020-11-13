0ac28950cfe77e4355d252290bdb9d54f5b44a16
wangsong <wangsong@ecsage.com>
2020-11-13 19:33:12 +0800

    #

--- bi_etl/sync/file/interface/get_async_tasks_status.py
+++ bi_etl/sync/file/interface/get_async_tasks_status.py
@@ -69,7 +69,7 @@ def get_async_status_content(MysqlSession="",Sql="",AsyncNotemptyFile="",AsyncEm
                                AsyncEmptyFile=AsyncEmptyFile,AsyncNotSuccFile=AsyncNotSuccFile,AsyncStatusExceptionFile=AsyncStatusExceptionFile
                            )
         if thread_id % 50 == 0:
-            time.sleep(20)
+            time.sleep(2)
         etl_thread.start()
         th.append(etl_thread)
         thread_id = thread_id + 1
