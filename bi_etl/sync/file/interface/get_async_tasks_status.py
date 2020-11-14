d6ae86181c2a65e756b0f25e13232596d78102cd
王松 <wangsong@ecsage.com>
2020-11-14 10:35:15 -0500

    Update get_async_tasks_status.py

--- bi_etl/sync/file/interface/get_async_tasks_status.py
+++ bi_etl/sync/file/interface/get_async_tasks_status.py
@@ -58,7 +58,7 @@ def get_async_status_content(MysqlSession="",Sql="",AsyncNotemptyFile="",AsyncEm
       AsyncNotSuccFile = arg["AsyncNotSuccFile"]
       MysqlSession = arg["MysqlSession"]
       ok,datas = etl_md.get_all_rows(Sql)
-      os.system("""echo "%s">>/tmp/gggg2222ggg.log"""%(data))
+      os.system("""echo "%s">>/tmp/gggg2222ggg.log"""%(datas))
     #th = []
     #thread_data = []
     #thread_id = 1
