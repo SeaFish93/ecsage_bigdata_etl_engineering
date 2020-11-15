a8c7d4d078f9899c71c1a92d6798f39187903259
王松 <wangsong@ecsage.com>
2020-11-15 11:15:19 -0500

    Update celeryconfig.py

--- bi_etl/sync/file/interface/celeryconfig.py
+++ bi_etl/sync/file/interface/celeryconfig.py
@@ -3,7 +3,7 @@ from kombu import Exchange
 #broker use rabbitmq
 BROKER_URL = 'redis://:1qazXSW2@192.168.30.235:9544/0'
 #backend use redis
-CELERY_RESULT_BACKEND = 'redis://:1qazXSW2@192.168.30.235:9544/1'
+#CELERY_RESULT_BACKEND = 'redis://:1qazXSW2@192.168.30.235:9544/1'
 #use json
 CELERY_RESULT_SERIALIZER = 'json'
 #task result expires
