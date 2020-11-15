d62e4d253a7fc34ad81c5c45eb9bbd1ff52c7170
王松 <wangsong@ecsage.com>
2020-11-15 07:31:01 -0500

    Add new file

---
+++ bi_etl/sync/file/interface/tylerscope.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, unicode_literals
+from celery import Celery, platforms
+import sys,os
+import time
+
+platforms.C_FORCE_ROOT = True
+
+sys.path.append(os.path.abspath("."))
+
+app = Celery('tasks',include=['tasks'])
+app.config_from_object('celeryconfig')
+
+def my_decorator(func):
+    def wrapper():
+        print('before the func runs')
+        func()
+        print('after the func runs')
+    return wrapper
+
+@my_decorator
+def infoo():
+    print('nh.....')
+
+if __name__ == '__main__':
+    app.start()
