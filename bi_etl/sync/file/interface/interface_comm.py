12639e3ab2fef5e1c973269999ebd65f30f8df0e
wangsong <wangsong@ecsage.com>
2020-11-30 16:17:54 +0800

    #

--- bi_etl/sync/file/interface/interface_comm.py
+++ bi_etl/sync/file/interface/interface_comm.py
@@ -64,7 +64,8 @@ def get_sync_data(ParamJson="",UrlPath=""):
     del param_json["service_code"]
     data_list = set_sync_data(ParamJson=param_json,UrlPath=UrlPath,Token=token)
     for data in data_list:
-        print(data.decode(),type(data.decode()),"===============================")
+        datas = eval(data.decode())
+        print(datas["data"],"===============================")
 
 #多线程上传hdfs
 def get_local_hdfs_thread(TargetDb="",TargetTable="",ExecDate="",DataFileList="",HDFSDir=""):
