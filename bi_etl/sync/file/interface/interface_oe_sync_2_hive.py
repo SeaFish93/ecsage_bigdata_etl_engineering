828270c1b93087e0380ee1a57389d07a642f6f62
wangsong <wangsong@ecsage.com>
2020-12-01 10:04:20 +0800

    #

--- bi_etl/sync/file/interface/interface_oe_sync_2_hive.py
+++ bi_etl/sync/file/interface/interface_oe_sync_2_hive.py
@@ -68,6 +68,8 @@ def get_sync_pages_number():
     from metadb.oe_sync_page_interface a where page_num > 0
   """
   ok,datas = etl_md.get_all_rows(sql)
+  print(type(param_json),"====================================")
+  exit(0)
   for dt in datas:
      page_number = int(dt[3])
      for page in range(page_number):
