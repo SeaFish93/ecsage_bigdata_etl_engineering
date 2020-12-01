b0c976b445f1161811bdf295a47c3180973e5122
wangsong <wangsong@ecsage.com>
2020-12-01 17:02:46 +0800

    #

--- bi_etl/sync/file/interface/celeryconfig.py
+++ bi_etl/sync/file/interface/celeryconfig.py
@@ -7,10 +7,10 @@ CELERY_RESULT_SERIALIZER = 'json'
 #task result expires
 CELERY_TASK_RESULT_EXPIRES = 60*60*24
 #task child killed after 40 times processing
-CELERYD_MAX_TASKS_PER_CHILD = 100
+CELERYD_MAX_TASKS_PER_CHILD = 40
 #celery worker amount
 #CELERY_CONCURRENCY = 20
-CELERYD_CONCURRENCY = 300
+CELERYD_CONCURRENCY = 20
 #the amount that a celery worker get task from broker each time
 CELERYD_PREFETCH_MULTIPLIER = 4
 CELERY_TASK_SERIALIZER = 'pickle'
