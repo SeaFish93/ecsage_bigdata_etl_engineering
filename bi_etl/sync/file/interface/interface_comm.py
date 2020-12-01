9a2da47b485c5f9386f43087815e696b96bff519
wangsong <wangsong@ecsage.com>
2020-12-01 17:17:30 +0800

    #

--- bi_etl/sync/file/interface/interface_comm.py
+++ bi_etl/sync/file/interface/interface_comm.py
@@ -237,16 +237,13 @@ def set_oe_async_tasks_data(DataFile="",ExecData="",AirflowInstance=""):
        else:
            os.system("""echo '%s %s'>>%s""" % (account_id,code, "/home/ecsage_data/oceanengine/async/%s/" % (media_type) + "account_sum_%s_%s" % (AirflowInstance,hostname)))
            if int(code) == 0:
-             #os.system("""echo '%s'>>%s""" % (account_id, "/home/ecsage_data/oceanengine/async/%s/" % (media_type) + "test_if_%s_%s" % (AirflowInstance, hostname)))
              for data in resp_datas:
-                 print(data, "==============================")
                  try:
                    os.system("""echo '%s'>>%s""" % (account_id, "/home/ecsage_data/oceanengine/async/%s/" % (media_type) + "test_%s_%s" % (AirflowInstance, hostname)))
                    shell_cmd = """
                    cat >> %s << endwritefilewwwww
 %s
 endwritefilewwwww"""%(DataFile+".%s"%(hostname),data.decode("utf8","ignore").replace("""`""","%%@@%%").replace("'","%%&&%%"))
-                   #os.system("""echo '%s'>>%s""" % (account_id, "/home/ecsage_data/oceanengine/async/%s/" % (media_type) + "test_%s_%s" % (AirflowInstance, hostname)))
                    os.system(shell_cmd)
                  except Exception as e:
                    os.system("""echo '%s'>>%s""" % (account_id, "/home/ecsage_data/oceanengine/async/%s/"%(media_type) + "write_exception_%s_%s" % (AirflowInstance,hostname)))
