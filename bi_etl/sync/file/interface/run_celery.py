7d70aea2ff3dba92d1a8a0d5b3d177902c1a09bb
wangsong <wangsong@ecsage.com>
2020-12-02 11:41:52 +0800

    #

--- bi_etl/sync/file/interface/run_celery.py
+++ bi_etl/sync/file/interface/run_celery.py
@@ -6,10 +6,10 @@ do
 done
 cd /root/bigdata_item_code/ecsage_bigdata_etl_engineering
 git pull
-rm -f /usr/local/python3/lib/python3.7/site-packages/airflow/logs/celery.log
-nohup /usr/local/python3/bin/celery -A ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tylerscope worker -l info >/usr/local/python3/lib/python3.7/site-packages/airflow/logs/celery.log 2>&1 &
-tail -f /usr/local/python3/lib/python3.7/site-packages/airflow/logs/celery.log
+rm -f /home/airflow/logs/celery.log
+nohup /usr/local/python3/bin/celery -A ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tylerscope worker -l info >/home/airflow/logs/celery.log 2>&1 &
+tail -f /home/airflow/logs/celery.log
 
-nohup /usr/redis/bin/redis-server /usr/redis/bin/redis.conf >/usr/local/python3/lib/python3.7/site-packages/airflow/logs/redis.log 2>&1 &
+nohup /usr/redis/bin/redis-server /usr/redis/bin/redis.conf >/home/airflow/logs/redis.log 2>&1 &
 /usr/redis/bin/redis-cli -h 192.168.30.17 -p 9543 -a 1qazXSW2 -n 0 ltrim transcode 0 196
 """
