2dd524f44229880d270b9f5cbcd412045701b95a
wangsong <wangsong@ecsage.com>
2020-12-03 18:22:26 +0800

    #

--- bi_etl/sync/file/interface/interface_comm.py
+++ bi_etl/sync/file/interface/interface_comm.py
@@ -132,6 +132,7 @@ def get_sync_data(ParamJson="",UrlPath="",TaskExceptionFile=""):
       data = "数据失败"
     log = Logger(filename="/home/ecsage_data/oceanengine/async/2/sync_data_file.log.%s"%(hostname))
     log.logger.info(data)
+    log.rotateHandler.close()
     return data,remark
 
 #多线程上传hdfs
