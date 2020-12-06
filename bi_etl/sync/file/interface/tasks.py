fdfb7215c02fa7a15362be945660434a5d22451d
wangsong <wangsong@1111.com>
2020-12-06 14:31:11 +0800

    ####

--- bi_etl/sync/file/interface/tasks.py
+++ bi_etl/sync/file/interface/tasks.py
@@ -15,6 +15,7 @@ from ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.interface_comm im
 from ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.interface_comm import set_oe_async_tasks_data_return
 from ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.interface_comm import get_write_local_file
 from ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.interface_comm import get_sync_data
+from ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.set_Logger import LogManager
 import json
 import ast
 import os
@@ -28,6 +29,9 @@ hostname = socket.gethostname()
 def get_test(string=""):
     now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime())
     print(now,"=================================")
+    test_log = LogManager("""get_test""" % (hostname)).get_logger_and_add_handlers(2,log_path='/home/ecsage_data/oceanengine/async/2',
+                                                                                              log_filename="""get_test.log""")
+    test_log.info(now)
 
 #定义oe任务创建
 @app.task(rate_limit='750/m')
