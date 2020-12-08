4726f1486eb0850894dad5b997f838de6291cdd6
wangsong <wangsong@ecsage.com>
2020-12-08 10:45:54 +0800

    #

--- bi_etl/sync/file/interface/interface_2_hive.py
+++ bi_etl/sync/file/interface/interface_2_hive.py
@@ -603,6 +603,7 @@ def exec_snap_hive_table(HiveSession="",BeelineSession="",SourceDB="",SourceTabl
            else:
                key_columns_join = "and a.`%s` = b.`%s`" % (key, key)
            key_columns_joins = key_columns_joins + " " + key_columns_join
+           num = num + 1
        #获取ods表字段
        ok,ods_table_columns = HiveSession.get_column_info(SourceDB,SourceTable)
        ods_columns = ""
--- bi_etl/sync/file/interface/interface_oe_sync_2_hive.py
+++ bi_etl/sync/file/interface/interface_oe_sync_2_hive.py
@@ -12,12 +12,7 @@ from ecsage_bigdata_etl_engineering.common.session.db_session import set_db_sess
 from ecsage_bigdata_etl_engineering.common.base.airflow_instance import Airflow
 from ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks import get_oe_sync_tasks_data_return as get_oe_sync_tasks_data_return_celery
 from ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks import get_oe_sync_tasks_data as get_oe_sync_tasks_data_celery
-from ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks import get_write_local_files as get_write_local_files_celery
 from ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.interface_comm import get_local_hdfs_thread
-from ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.get_account_tokens import get_oe_account_token
-from ecsage_bigdata_etl_engineering.common.base.sync_method import get_table_columns_info
-from ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.get_data_2_snap import exec_snap_hive_table
-from ecsage_bigdata_etl_engineering.common.base.etl_thread import EtlThread
 from ecsage_bigdata_etl_engineering.common.base.get_config import Conf
 import os
 import time
@@ -34,11 +29,16 @@ def main(TaskInfo,Level="",**kwargs):
     global developer
     global regexp_extract_column
     airflow = Airflow(kwargs)
-    media_type = TaskInfo[1]
-    task_type = TaskInfo[4]
     print(TaskInfo,"####################@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
     exec_date = airflow.execution_date_utc8_str[0:10]
-    get_sync_interface_2_local(AirflowDag=airflow.dag, AirflowTask=airflow.task, TaskInfo=TaskInfo, ExecDate=exec_date)
+    target_db = TaskInfo[14]
+    target_table = TaskInfo[15]
+    hive_session = set_db_session(SessionType="hive", SessionHandler="hive")
+    beeline_session = set_db_session(SessionType="beeline", SessionHandler="beeline")
+    if Level == "file":
+       get_sync_interface_2_local(BeelineSession=beeline_session,TargetDB=target_db,TargetTable=target_table,
+                                  AirflowDag=airflow.dag, AirflowTask=airflow.task,
+                                  TaskInfo=TaskInfo, ExecDate=exec_date)
 
 def set_sync_pages_number(DataList="",ParamJson="",UrlPath="",SyncDir="",PageTaskFile="",CelerySyncTaskFile="",DataFileDir="",DataFile="",IsFilter=""):
     param_json = ParamJson
@@ -64,7 +64,7 @@ def set_sync_pages_number(DataList="",ParamJson="",UrlPath="",SyncDir="",PageTas
     columns = """page_num,account_id,service_code,remark,data,request_filter,flag"""
     load_data_mysql(AsyncAccountFile=SyncDir, DataFile=PageTaskFile, TableName="oe_sync_page_interface",Columns=columns)
 
-def get_sync_interface_2_local(AirflowDag="",AirflowTask="",TaskInfo="",ExecDate=""):
+def get_sync_interface_2_local(BeelineSession="",TargetDB="",TargetTable="",AirflowDag="",AirflowTask="",TaskInfo="",ExecDate=""):
   local_time = time.strftime("%Y-%m-%d_%H_%M_%S", time.localtime())
   local_dir = """/home/ecsage_data/oceanengine/sync/%s/%s/%s"""%(ExecDate,AirflowDag,AirflowTask)
   celery_get_page_status = """%s/celery_get_page_status.log"""%(local_dir)
@@ -74,13 +74,6 @@ def get_sync_interface_2_local(AirflowDag="",AirflowTask="",TaskInfo="",ExecDate
   tmp_data_task_file = """%s/tmp_data_task_file.log""" % (local_dir)
   task_exception_file = "%s/task_exception_file.log"%(local_dir)
   param_json = ast.literal_eval(json.loads(json.dumps(TaskInfo[5])))
-  ######param_json = {"end_date": "2020-12-06", "page_size": "1000", "start_date": "2020-12-06",
-  ######             "advertiser_id": "", "group_by": ['STAT_GROUP_BY_FIELD_ID', 'STAT_GROUP_BY_CITY_NAME'],
-  ######             "time_granularity": "STAT_TIME_GRANULARITY_DAILY",
-  ######             "page": 1,
-  ######             "filtering": {"campaign_ids": "","status":"CREATIVE_STATUS_ALL"},
-  ######             "service_code": "data[2]"
-  ######             }
   url_path = TaskInfo[4]
   filter_db_name = TaskInfo[21]
   filter_table_name = TaskInfo[22]
@@ -142,14 +135,6 @@ def get_sync_interface_2_local(AirflowDag="",AirflowTask="",TaskInfo="",ExecDate
    where tmp1.remark = '异常'
      and tmp1.flag = '%s.%s'
    group by tmp1.account_id, tmp1.service_code,tmp1.request_filter,tmp1.request_filter,tmp1.flag
-      union all
-   select account_id, '222' media_type, service_code,trim(replace(replace(request_filter,'[',''),']','')),flag
-   from metadb.oe_sync_page_interface a
-   where page_num = 0
-     and remark = '正常'
-     and flag = '%s.%s'
-     and (data is null or length(data) = 0)
-  group by account_id, service_code,request_filter,request_filter,flag
   """%(AirflowDag,AirflowTask,AirflowDag,AirflowTask,AirflowDag,AirflowTask)
     ok, db_data = etl_md.get_all_rows(sql)
     if db_data is not None and len(db_data) > 0:
@@ -197,40 +182,76 @@ def get_sync_interface_2_local(AirflowDag="",AirflowTask="",TaskInfo="",ExecDate
      celery_task_id, status_wait = get_celery_status_list(CeleryTaskStatusFile=celery_get_data_status)
      wait_for_celery_status(StatusList=celery_task_id)
      print("celery队列执行完成！！！%s"%(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+     #获取数据文件
+     target_file = os.listdir(local_dir)
+     data_task_file_list = []
+     for files in target_file:
+         if data_task_file.split("/")[-1] in files:
+             data_task_file_list.append((local_dir, files))
+     #数据落地至etl_mid
+     load_data_2_etl_mid(BeelineSession=BeelineSession, LocalFileList=data_task_file_list, TargetDB=TargetDB,
+                         TargetTable=TargetTable, ExecDate=ExecDate)
 
-def run_thread(StatusFile="",DataLocalFile="",WriteLocalFilesStauts="",arg=None):
-   if arg is not None and len(arg)>0:
-    StatusFile = arg["StatusFile"]
-    DataLocalFile = arg["DataLocalFile"]
-    #AccountId = arg["AccountId"]
-    WriteLocalFilesStauts = arg["WriteLocalFilesStauts"]
-    with open(StatusFile) as lines:
-        array = lines.readlines()
-        for data in array:
-            get_data1 = data.strip('\n').split(" ")
-            get_celery_job_data(CeleryTaskId=get_data1[0],AccountId=get_data1[1],DataLocalFile=DataLocalFile,WriteLocalFilesStauts=WriteLocalFilesStauts)
-
-def get_celery_job_data(CeleryTaskId="",AccountId="",DataLocalFile="",WriteLocalFilesStauts=""):
-    pass
-    #task_write_id = get_write_local_files_celery.delay(CeleryTaskId=CeleryTaskId,AccountId=AccountId,DataLocalFile=DataLocalFile)
-    #os.system("""echo '%s'>>%s """%(task_write_id,WriteLocalFilesStauts))
-
-def rerun_data():
-    sql = """
-           select tmp1.account_id, '222' media_type, tmp1.service_code
-           from(select account_id,service_code,count(distinct remark) as rn
-                from metadb.oe_sync_page_interface
-                group by account_id,service_code
-                having count(distinct remark) = 1
-               ) tmp
-           inner join metadb.oe_sync_page_interface tmp1
-           on tmp.account_id = tmp1.account_id
-           and tmp.service_code = tmp1.service_code
-           where tmp1.remark = '异常'
-           group by tmp1.account_id, tmp1.service_code
-        """
-    ok, db_data = etl_md.get_all_rows(sql)
-    return db_data
+def load_data_2_etl_mid(BeelineSession="",LocalFileList="",TargetDB="",TargetTable="",ExecDate=""):
+    if LocalFileList is None and len(LocalFileList) == 0:
+        msg = get_alert_info_d(DagId=airflow.dag, TaskId=airflow.task,
+                               SourceTable="%s.%s" % ("SourceDB", "SourceTable"),
+                               TargetTable="%s.%s" % (TargetDB, TargetTable),
+                               BeginExecDate=ExecDate,
+                               EndExecDate=ExecDate,
+                               Status="Error",
+                               Log="API采集没执行！！！",
+                               Developer="developer")
+        set_exit(LevelStatu="red", MSG=msg)
+    mid_sql = """
+        drop table if exists %s.%s;
+        create table if not exists %s.%s
+        (
+         request_data string
+        )partitioned by(etl_date string,request_type string,delete_type string)
+        row format delimited fields terminated by '\\001' 
+        ;
+    """ % (TargetDB,TargetTable,TargetDB,TargetTable)
+    BeelineSession.execute_sql(mid_sql)
+    load_num = 0
+    hdfs_dir = conf.get("Airflow_New", "hdfs_home")
+    load_table_sqls = ""
+    load_table_sql_0 = ""
+    load_table_sql = ""
+    for data in LocalFileList:
+        local_file = """%s""" % (data)
+        # 落地mid表
+        if load_num == 0:
+            load_table_sql_0 = """
+                         load data inpath '{hdfs_dir}/{file_name}' OVERWRITE  INTO TABLE {target_db}.{target_table}
+                         partition(etl_date='{exec_date}')
+                         ;\n
+            """.format(hdfs_dir=hdfs_dir, file_name=local_file.split("/")[-1], target_db=TargetDB,
+                       target_table=TargetTable,exec_date=ExecDate)
+        else:
+            load_table_sql = """
+                         load data inpath '{hdfs_dir}/{file_name}' INTO TABLE {target_db}.{target_table}
+                         partition(etl_date='{exec_date}')
+                         ;\n
+                     """.format(hdfs_dir=hdfs_dir, file_name=local_file.split("/")[-1],
+                                target_db=TargetDB,target_table=TargetTable,exec_date=ExecDate)
+        load_table_sqls = load_table_sql + load_table_sqls
+        load_num = load_num + 1
+    load_table_sqls = load_table_sql_0 + load_table_sqls
+    # 上传hdfs
+    get_local_hdfs_thread(TargetDb=TargetDB, TargetTable=TargetTable, ExecDate=ExecDate, DataFileList=LocalFileList,HDFSDir=hdfs_dir)
+    # 落地至hive
+    ok_data = BeelineSession.execute_sql(load_table_sqls)
+    if ok_data is False:
+        msg = get_alert_info_d(DagId=airflow.dag, TaskId=airflow.task,
+                               SourceTable="%s.%s" % ("SourceDB", "SourceTable"),
+                               TargetTable="%s.%s" % (TargetDB, TargetTable),
+                               BeginExecDate=ExecDate,
+                               EndExecDate=ExecDate,
+                               Status="Error",
+                               Log="HDFS数据文件load入仓临时表出现异常！！！",
+                               Developer="developer")
+        set_exit(LevelStatu="red", MSG=msg)
 
 def load_data_mysql(AsyncAccountFile="",DataFile="",TableName="",Columns=""):
     target_file = os.listdir(AsyncAccountFile)
@@ -306,72 +327,3 @@ def wait_for_celery_status(StatusList=""):
           run_wait = False
       status_false.clear()
       sleep_num = sleep_num + 1
-
-def rerun_exception_create_tasks(AsyncAccountDir="",ExceptionFile="",DataFile="",CeleryTaskDataFile="",LogSession="",InterfaceFlag="",ExecDate=""):
-    async_data_file = """%s/%s"""%(AsyncAccountDir,DataFile.split("/")[-1])
-    celery_task_data_file = """%s/%s"""%(AsyncAccountDir,CeleryTaskDataFile.split("/")[-1])
-    async_data_exception_file = """%s/%s""" % (AsyncAccountDir, ExceptionFile.split("/")[-1])
-    #先保留第一次
-    delete_sql = """delete from metadb.oe_async_exception_create_tasks_interface where interface_flag = '%s' """ % (InterfaceFlag)
-    etl_md.execute_sql(delete_sql)
-    columns = """account_id,interface_flag,media_type,service_code,group_by,fields,token_data"""
-    table_name = "oe_async_exception_create_tasks_interface"
-    save_exception_tasks(AsyncAccountDir=AsyncAccountDir,ExceptionFile=ExceptionFile,TableName=table_name,Columns=columns)
-    #
-    n = 3
-    for i in range(n):
-        sql = """
-          select distinct a.account_id,a.interface_flag,a.media_type,a.service_code,a.group_by,a.fields,a.token_data
-          from metadb.oe_async_exception_create_tasks_interface a
-          where interface_flag = '%s'
-        """% (InterfaceFlag)
-        ok,datas = etl_md.get_all_rows(sql)
-        if datas is not None and len(datas) > 0:
-           print("开始第%s次重试异常，时间：%s"%(i+1,time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-           for data in datas:
-               pass
-               ###status_id = get_oe_async_tasks_create_celery.delay(AsyncTaskName="%s" % (i), AsyncTaskFile=async_data_file,
-               ###                                               AsyncTaskExceptionFile=async_data_exception_file,
-               ###                                               ExecData=data, ExecDate=ExecDate)
-               #os.system("""echo "%s %s">>%s""" % (status_id, data[0], celery_task_data_file+".%s"%(i)))
-           celery_task_id, status_wait = get_celery_status_list(CeleryTaskStatusFile=celery_task_data_file + ".%s"%i)
-           wait_for_celery_status(StatusList=celery_task_id)
-           delete_sql = """delete from metadb.oe_async_exception_create_tasks_interface where interface_flag = '%s' """ % (InterfaceFlag)
-           etl_md.execute_sql(delete_sql)
-           save_exception_tasks(AsyncAccountDir=AsyncAccountDir, ExceptionFile=ExceptionFile, TableName=table_name,Columns=columns)
-           print("结束第%s次重试异常，时间：%s" % (i + 1, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-           #判断结果是否还有异常
-           ex_sql = """
-                     select a.account_id,a.interface_flag,a.media_type,a.service_code,a.group_by,a.fields,a.token_data
-                     from metadb.oe_async_exception_create_tasks_interface a
-                     where interface_flag = '%s'
-                     limit 1
-              """% (InterfaceFlag)
-           ok, ex_datas = etl_md.get_all_rows(ex_sql)
-           if ex_datas is not None and len(ex_datas) > 0:
-               print("休眠中...，时间：%s" % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-               if i == 0:
-                 time.sleep(360)
-               else:
-                 time.sleep(180)
-    ex_sql = """
-         select a.account_id,a.interface_flag,a.media_type,a.service_code,a.group_by,a.fields,a.token_data
-         from metadb.oe_async_exception_create_tasks_interface a
-         where interface_flag = '%s'
-    """% (InterfaceFlag)
-    ok, ex_datas = etl_md.get_all_rows(ex_sql)
-    if ex_datas is not None and len(ex_datas) > 0:
-        print("还有特别异常任务存在！！！")
-        print(ex_datas[0])
-
-def save_exception_tasks(AsyncAccountDir="",ExceptionFile="",TableName="",Columns=""):
-    exception_file = ExceptionFile.split("/")[-1]
-    exception_file_list = []
-    target_file = os.listdir(AsyncAccountDir)
-    for files in target_file:
-      if exception_file in files:
-         exception_file_list.append((AsyncAccountDir, files))
-    if exception_file_list is not None and len(exception_file_list) > 0 :
-       for file in exception_file_list:
-           load_data_mysql(AsyncAccountFile=file[0], DataFile=file[1],TableName=TableName, Columns=Columns)
-           os.system("""rm -f %s/%s"""%(file[0],file[1]))
