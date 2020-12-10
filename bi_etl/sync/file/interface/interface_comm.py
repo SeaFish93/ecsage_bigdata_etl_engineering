4759261fa11c70c3c4aa31bca97078df822fdd68
wangsong <wangsong@ecsage.com>
2020-12-10 20:34:05 +0800

    #

--- bi_etl/sync/file/interface/interface_comm.py
+++ bi_etl/sync/file/interface/interface_comm.py
@@ -519,7 +519,7 @@ def get_creative_detail_datas(ParamJson="", UrlPath="", DataFileDir="", DataFile
                 # 没权限及token失败
                 if int(code) in [40002, 40105, 40104]:
                     code = 0
-                    os.system(""" echo "%s">>%s/%s.%s """ % (str(data_list).replace(" "), DataFileDir, "account_status.log", hostname))
+                    os.system(""" echo "%s">>%s/%s.%s """ % (str(data_list).replace(" ",""), DataFileDir, "account_status.log", hostname))
                 else:
                     code = 1
         except Exception as e:
