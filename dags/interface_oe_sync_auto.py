5f7d05f83962bdb3132c4fdfb346c438f43a164b
wangsong <wangsong@ecsage.com>
2020-12-18 14:17:58 +0800

    #

--- dags/interface_oe_sync_auto.py
+++ dags/interface_oe_sync_auto.py
@@ -115,8 +115,7 @@ for dag_info in get_dags:
                               task[task_dep[1]].set_upstream(start_sync_task)
                       else:
                           external_task_id = 'external_%s_%s' % (task_dep[0], task_dep[1])
-                          if external_task_id in list(external_task.keys()) and external_task[
-                              external_task_id].dag_id == dag_id:
+                          if external_task_id in list(external_task.keys()) and external_task[external_task_id].dag_id == dag_id:
                               task[task_dep[2]].set_upstream(external_task[external_task_id])
                           else:
                               external_task['%s' % (external_task_id)] = PythonOperator(task_id=external_task_id,
