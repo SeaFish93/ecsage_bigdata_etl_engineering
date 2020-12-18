47e3be4abf42cd13a0c158407a49f00ef3c3475a
wangsong <wangsong@ecsage.com>
2020-12-18 11:32:24 +0800

    #

--- bi_etl/sync/file/interface/interface_comm.py
+++ bi_etl/sync/file/interface/interface_comm.py
@@ -159,6 +159,7 @@ def get_sync_data(ParamJson="",UrlPath="",DataFileDir="",DataFile=""):
 
 #多线程上传hdfs
 def get_local_hdfs_thread(TargetDb="",TargetTable="",ExecDate="",DataFileList="",HDFSDir=""):
+    print(DataFileList,"=========================================")
     th = []
     i = 0
     file_num = 0
