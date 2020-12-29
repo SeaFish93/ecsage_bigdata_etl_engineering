18e4f9c93fa8b6531f76cb58046f012afc52b7f4
wangsong <wangsong@ecsage.com>
2020-12-29 18:07:16 +0800

    ##

--- bi_etl/sync/file/interface/tasks.py
+++ bi_etl/sync/file/interface/tasks.py
@@ -343,7 +343,7 @@ def get_not_page(UrlPath="",ParamJson="",ServiceCode="",Token="",ReturnAccountId
       n = n + 1
 
 #处理分页
-@app.task(rate_limit='500/m')
+@app.task(rate_limit='1000/m')
 def get_pages(UrlPath="",ParamJson="",ServiceCode="",Token="",DataFileDir="",DataFile="",ReturnAccountId="",TaskFlag="",PageTaskFile="",TaskExceptionFile=""):
     set_true = True
     n = 0
