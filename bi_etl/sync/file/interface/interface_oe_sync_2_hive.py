449a7e601aefed5638a758ea678f0c822cf9193e
wangsong <wangsong@ecsage.com>
2021-01-05 15:53:26 +0800

    ##

--- bi_etl/sync/file/interface/interface_oe_sync_2_hive.py
+++ bi_etl/sync/file/interface/interface_oe_sync_2_hive.py
@@ -114,6 +114,7 @@ def get_data_2_etl_mid(BeelineSession="",TargetDB="",TargetTable="",AirflowDag="
   if filter_time is not None and len(filter_time) > 0:
       filter_time_sql = """ and %s >= '%s 00:00:00' and %s <= '%s 23:59:59' """%(filter_time,ExecDate,filter_time,ExecDate)
   os.system("""mkdir -p %s"""%(local_dir))
+  os.system("""chmod -R 777 %s""" % (local_dir))
   os.system("""rm -f %s/*"""%(local_dir))
   #判断是否从列表过滤
   if filter_db_name is not None and len(filter_db_name) > 0:
