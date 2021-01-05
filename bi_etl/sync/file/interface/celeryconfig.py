debc8b0d4ca1a7e08ed44be371eede51ecf4144c
王松 <wangsong@ecsage.com>
2021-01-05 02:38:26 -0500

    Update celeryconfig.py

--- bi_etl/sync/file/interface/celeryconfig.py
+++ bi_etl/sync/file/interface/celeryconfig.py
@@ -1,7 +1,7 @@
-#broker use redis
-BROKER_URL = 'redis://:1qazXSW2@192.168.30.17:9543/0'
-#backend use redis
-CELERY_RESULT_BACKEND = 'redis://:1qazXSW2@192.168.30.17:9543/1'
+#broker use redis 测试库
+BROKER_URL = 'redis://:1qazXSW2@192.168.30.17:9543/99'
+#backend use redis 测试库
+CELERY_RESULT_BACKEND = 'redis://:1qazXSW2@192.168.30.17:9543/100'
 #use json
 CELERY_RESULT_SERIALIZER = 'json'
 #task result expires
@@ -10,7 +10,7 @@ CELERY_TASK_RESULT_EXPIRES = 60*60*24
 CELERYD_MAX_TASKS_PER_CHILD = 40
 #celery worker amount
 #CELERY_CONCURRENCY = 20
-CELERYD_CONCURRENCY = 300
+CELERYD_CONCURRENCY = 10
 #the amount that a celery worker get task from broker each time
 CELERYD_PREFETCH_MULTIPLIER = 4
 CELERY_TASK_SERIALIZER = 'pickle'
