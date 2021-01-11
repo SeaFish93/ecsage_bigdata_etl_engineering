b5b6cf55e45aa2874b60986dd8cf93d9dfb60aa2
wangsong <wangsong@ecsage.com>
2021-01-11 14:36:25 +0800

    ##

--- bi_etl/sync/file/interface/run_celery.py
+++ bi_etl/sync/file/interface/run_celery.py
@@ -16,5 +16,6 @@ nohup /usr/redis/bin/redis-server /usr/redis/bin/redis.conf >/home/airflow/logs/
 /usr/redis/bin/redis-cli -h 192.168.30.17 -p 9543 -a 1qazXSW2 -n 0 ltrim transcode 0 196
 
 nohup /usr/local/python3/bin/celery flower -A ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tylerscope --broker=redis://:1qazXSW2@192.168.30.17:9543/9 --port=9544 >/home/airflow/logs/flower.log 2>&1 &
+/usr/local/python3/bin/celery flower -A ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tylerscope --broker_api=http://root:1qazXSW2@192.168.30.127:9548/api/ --port=9544
 
 """
