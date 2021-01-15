bf4eef623930358af9e79a400ebd71eb4180cf76
王松 <wangsong@ecsage.com>
2021-01-15 11:44:29 -0500

    Update celeryconfig.py

--- bi_etl/sync/file/interface/celeryconfig.py
+++ bi_etl/sync/file/interface/celeryconfig.py
@@ -1,8 +1,8 @@
 from kombu import Queue, Exchange
 
 BROKER_URL = 'amqp://root:1qazXSW2@192.168.30.17:9549//sync'
-#CELERY_RESULT_BACKEND = 'db+mysql://root:Yk@123@192.168.30.235:3306/sync'
-CELERY_RESULT_BACKEND = 'redis://:1qazXSW2@192.168.30.17:9543/5'
+CELERY_RESULT_BACKEND = 'db+mysql://root:Yk@123@192.168.30.235:3306/sync'
+#CELERY_RESULT_BACKEND = 'redis://:1qazXSW2@192.168.30.17:9543/5'
 #use json
 #CELERY_RESULT_SERIALIZER = 'json'
 #task result expires
