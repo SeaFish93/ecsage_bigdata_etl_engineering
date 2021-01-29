5d41e875905de8d61d3eee800f4346c755944d5b
wangsong <wangsong@ecsage.com>
2021-01-29 15:50:01 +0800

    ##

--- bi_etl/sync/file/interface/tylerscope.py
+++ bi_etl/sync/file/interface/tylerscope.py
@@ -10,17 +10,7 @@ sys.path.append(os.path.abspath("."))
 
 app = Celery('ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks',include=['ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks'])
 app.config_from_object('ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.celeryconfig')
-#app.autodiscover_tasks(packages='ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface',related_name='tasks')
-##### def my_decorator(func):
-#####     def wrapper():
-#####         print('before the func runs')
-#####         func()
-#####         print('after the func runs')
-#####     return wrapper
-##### 
-##### @my_decorator
-##### def infoo():
-#####     print('nh.....')
+app.autodiscover_tasks(packages='ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface',related_name='tasks')
 
 if __name__ == '__main__':
     app.start()
