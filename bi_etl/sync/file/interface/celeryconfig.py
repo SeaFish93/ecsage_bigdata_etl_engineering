1dafc7373217df84addc056dd03485492738e40e
wangsong <wangsong@ecsage.com>
2021-02-02 16:39:00 +0800

    ##

--- bi_etl/sync/file/interface/celeryconfig.py
+++ bi_etl/sync/file/interface/celeryconfig.py
@@ -1,8 +1,8 @@
 from kombu import Queue, Exchange
 
-BROKER_URL = 'amqp://admin:1qazXSW2@192.168.30.123:9549//sync_broker'
+BROKER_URL = 'amqp://admin:1qazXSW2@192.168.30.130:9549//sync_broker'
 #CELERY_RESULT_BACKEND = 'file:///data/ecsage_data/celery/sync_backend'
-CELERY_RESULT_BACKEND = 'amqp://admin:1qazXSW2@192.168.30.123:9549//sync_backend'
+CELERY_RESULT_BACKEND = 'db+mysql://root:06D567130266EB33098B9F@192.168.30.5:13306/sync'
 #use json
 #CELERY_RESULT_SERIALIZER = 'json'
 #task result expires
