cb204bbd4c5ed0ab82360614888c99edef8356f2
王松 <wangsong@ecsage.com>
2021-02-02 21:14:16 -0500

    Update celeryconfig.py

--- bi_etl/sync/file/interface/celeryconfig.py
+++ bi_etl/sync/file/interface/celeryconfig.py
@@ -1,8 +1,8 @@
 from kombu import Queue, Exchange
 
-BROKER_URL = 'amqp://admin:1qazXSW2@192.168.30.130:9549//sync_broker'
+BROKER_URL = 'amqp://admin:1qazXSW2@192.168.30.130:9549//sync_broker_test'
 #CELERY_RESULT_BACKEND = 'file:///data/ecsage_data/celery/sync_backend'
-CELERY_RESULT_BACKEND = 'db+mysql://root:06D567130266EB33098B9F@192.168.30.5:13306/sync'
+CELERY_RESULT_BACKEND = 'db+mysql://root:Yk@123@192.168.30.235/sync'
 #use json
 #CELERY_RESULT_SERIALIZER = 'json'
 #task result expires
@@ -11,7 +11,7 @@ CELERY_RESULT_EXPIRES = 60*60*24
 CELERYD_MAX_TASKS_PER_CHILD = 5
 #celery worker amount
 #CELERY_CONCURRENCY = 20
-CELERYD_CONCURRENCY = 300
+CELERYD_CONCURRENCY = 10
 #the amount that a celery worker get task from broker each time
 CELERYD_PREFETCH_MULTIPLIER = 4
 CELERY_TASK_SERIALIZER = 'pickle'
@@ -30,22 +30,22 @@ CELERY_ACKS_LATE=True
 # worker进程崩掉之后拒绝确认
 CELERY_REJECT_ON_WORKER_LOST=True
 CELERY_QUEUES = (
-Queue('report', Exchange('report'), routing_key='report', consumer_arguments={'x-priority': 10}),
-Queue('oe', Exchange('oe'), routing_key='oe', consumer_arguments={'x-priority': 100}),
-Queue('tc', Exchange('tc'), routing_key='tc', consumer_arguments={'x-priority': 100})
+Queue('report_test', Exchange('report_test'), routing_key='report_test', consumer_arguments={'x-priority': 10}),
+Queue('oe_test', Exchange('oe_test'), routing_key='oe_test', consumer_arguments={'x-priority': 100}),
+Queue('tc_test', Exchange('tc_test'), routing_key='tc_test', consumer_arguments={'x-priority': 100})
 )
 CELERY_DEFAULT_QUEUE = 'default'
 CELERY_DEFAULT_EXCHANGE = 'default'
 CELERY_DEFAULT_ROUTING_KEY = 'default'
 CELERY_ROUTES = {
-'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_not_page_tc': {'queue': 'tc'},
-'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_pages_tc': {'queue': 'tc'},
-'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_pages': {'queue': 'oe'},
-'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_not_page': {'queue': 'oe'},
-'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_oe_async_tasks_data_return': {'queue': 'oe'},
-'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_oe_create_async_tasks': {'queue': 'oe'},
-'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_oe_status_async_tasks': {'queue': 'oe'},
-'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_service_page_data': {'queue': 'oe'},
-'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_service_data': {'queue': 'oe'},
-'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_web_interface_data': {'queue': 'report'}
+'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_not_page_tc': {'queue': 'tc_test'},
+'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_pages_tc': {'queue': 'tc_test'},
+'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_pages': {'queue': 'oe_test'},
+'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_not_page': {'queue': 'oe_test'},
+'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_oe_async_tasks_data_return': {'queue': 'oe_test'},
+'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_oe_create_async_tasks': {'queue': 'oe_test'},
+'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_oe_status_async_tasks': {'queue': 'oe_test'},
+'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_service_page_data': {'queue': 'oe_test'},
+'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_service_data': {'queue': 'oe_test'},
+'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_web_interface_data': {'queue': 'report_test'}
 }
