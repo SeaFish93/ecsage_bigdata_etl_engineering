e2ff5eee0b1a28718b1f8b7c41c0177ff645cd21
wangsong <wangsong@1111.com>
2021-02-08 23:53:03 +0800

    ####

--- bi_etl/sync/file/interface/interface_tc_sync_2_hive.py
+++ bi_etl/sync/file/interface/interface_tc_sync_2_hive.py
@@ -595,7 +595,8 @@ def rerun_exception_tasks_pages(DataFileDir="",ExceptionFile="",DataFile="",Page
              if IsPage == "Y":
                 if InterfaceFlag.split(".")[0] in ["day_tc_sync_targeting_tag_reports_region_travel_in"]:
                     if account_id == data[3] or nn == 1:
-                       param_json["filtering"][0]["values"].append(param_json["filtering"][0]["values"][0])
+                       if param_json["filtering"][0]["values"][0] not in param_json["filtering"][0]["values"]:
+                          param_json["filtering"][0]["values"].append(param_json["filtering"][0]["values"][0])
                     else:
                        status_id = get_targeting_pages_tc_celery.delay(UrlPath=data[0], ParamJson=param_json, ServiceCode=data[2],
                                                        Token=data[5],
