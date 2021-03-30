03624097fdc1b1ec7b2be8d2a704dd813fb499fd
wangsong <wangsong@ecsage.com>
2021-03-30 17:45:40 +0800

    ##

--- bi_etl/sync/file/interface/interface_oe_sync_2_hive.py
+++ bi_etl/sync/file/interface/interface_oe_sync_2_hive.py
@@ -717,7 +717,7 @@ def wait_for_celery_status(StatusList="",RequestRows="",TaskFlag=""):
     sleep_num = 1
     while run_wait:
       # 判断请求个数是否与请求完成个数一致
-      sql = """select count(1) from metadb.celery_sync_status where task_id = '%s' """ % (TaskFlag)
+      sql = """select count(distinct status_id) from metadb.celery_sync_status where task_id = '%s' """ % (TaskFlag)
       ok, request_task_finish_rows = etl_md.get_all_rows(sql=sql)
       if ok:
           print("等待完成个数：【源数%s】【目标数%s】" % (int(RequestRows), int(request_task_finish_rows[0][0])))
