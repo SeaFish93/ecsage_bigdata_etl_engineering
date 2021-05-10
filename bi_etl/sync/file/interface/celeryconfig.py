50a49ed88b09a3ae8187e4f45cc7f1dd780a4960
蒋杰 <Jiang5633035>
2021-05-10 15:45:06 +0800

    小时数据采集

--- bi_etl/sync/file/interface/celeryconfig.py
+++ bi_etl/sync/file/interface/celeryconfig.py
@@ -54,5 +54,9 @@ CELERY_ROUTES = {
 'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_web_interface_data': {'queue': 'report'},
 'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_tc_add_async_tasks': {'queue': 'tc'},
 'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_tc_status_async_tasks': {'queue': 'tc'},
-'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_tc_async_tasks_data_return': {'queue': 'tc'}
+'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_tc_async_tasks_data_return': {'queue': 'tc'},
+'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_pages_hour': {'queue': 'hour'},
+'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_not_page_hour': {'queue': 'hour'},
+'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_service_page_data_hour': {'queue': 'hour'},
+'ecsage_bigdata_etl_engineering.bi_etl.sync.file.interface.tasks.get_service_data_hour': {'queue': 'hour'}
 }
